ffa0eb753308559ef3cc58814e29cdc50118583b
Edric723 <gwriter723@gmail.com>
2024-05-20 15:30:39 -0300

    Botón ir Arriba
    -Implementación botón.

--- script.js
+++ script.js
@@ -91,5 +91,26 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// BOTÓN IR ARRIBA
 
+document.addEventListener("DOMContentLoaded", function() {
+    
+    
+    // ... Oculta el botón de "Ir arriba" al cargar la página
+    document.getElementById("boton-arriba").style.display = "none";
+
+    // Mostrar u ocultar el botón de "Ir arriba" al hacer scroll
+    window.addEventListener("scroll", function() {
+        var botonArriba = document.getElementById("boton-arriba");
+        var seccionInicio = document.getElementById("inicio");
+        var scrollY = window.scrollY || window.pageYOffset;
+
+        // Si el scroll está más abajo de la sección de inicio, mostrar el botón
+        if (scrollY > seccionInicio.offsetHeight) {
+            botonArriba.style.display = "block";
+        } else {
+            botonArriba.style.display = "none";
+        }
+    });
+});
 
